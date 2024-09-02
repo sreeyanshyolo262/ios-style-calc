ff36ed6e479a5be9ff64d4c4a796d8d20499a7d7
sreeyansh dhenavahi <149941096+sreeyanshyolo262@users.noreply.github.com>
2024-09-03 01:04:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+//calculator program
+
+const display=document.getElementById("display");
+
+function appendtoDisplay(input){
+    display.value += input;
+
+}
+function clearDisplay(){
+    display.value = "";
+
+}
+
+function calculate(){
+    display.value= eval(display.value);
+
+}
